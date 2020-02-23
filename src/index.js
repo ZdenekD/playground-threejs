8b91d764a38c6dace4548142059a345bbfdba80c
zdenekd <ditezdenek@gmail.com>
2020-02-23 12:55:13 +0100

    Add base page files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+const app = document.getElementById('js_main');
+
+if (app) {
+    console.log('There\'s no application element');
+}
+
+console.info('%cHello Underworld! Authorship is attributed to %c01011010 01100100 01100101 01101110 01100101 01101011 01000100. %cYou can try this >> https://bit.ly/31QKTyy', 'color: #9cd9f2', 'color: #c8e49b', 'color: #9cd9f2');
